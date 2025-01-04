5acaaf03260cb6a35cc46d59012f544fdc23ecb0
21adamse <EwanAd2010@gmail.com>
2025-01-04 16:11:50 +0000

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,6 @@
+numbers=[1,3,5,7,9,11,13,15,17,19]
+number=int(input("Enter a number"))
+if number in numbers:
+    print("That number is in the list")
+else:
+    print("That number is not in the listd")
